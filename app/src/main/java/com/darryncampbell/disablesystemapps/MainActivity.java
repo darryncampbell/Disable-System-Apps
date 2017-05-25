578b78a7c4d9ed22ea74a4cd1b043bcd9392b70a
darryncampbell <jrq768@zebra.com>
2017-05-25 15:06:40 +0100

    Will build the list of applications to be disabled / enabled from the applications currently on the device.

--- app/src/main/java/com/darryncampbell/disablesystemapps/MainActivity.java
+++ app/src/main/java/com/darryncampbell/disablesystemapps/MainActivity.java
@@ -1,5 +1,6 @@
 package com.darryncampbell.disablesystemapps;
 
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +12,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 //  Uncomment this import statement if you have the authoritative list of GMS applications.
+//  (autoDetectApplications will also need to be set to false)
 //import com.darryncampbell.disablesystemapps.UNDER_NDA.PackagesList;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity  {
+
+    //  Set this to true to use the PackageManager to generate the list of system apps to present to the user.
+    boolean autoDetectApplications = true;
 
     private static final String LOG_TAG = "Disable System Apps";
     private ArrayList<ListRow> packageEnabledList = new ArrayList<>();
@@ -99,44 +105,56 @@ public class MainActivity extends AppCompatActivity  {
 
     public void configureApplication()
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT &&
-                Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+        if (autoDetectApplications)
         {
-            operating_system = "KitKat";
-            selectedPackages = PackagesList.kitkat_packages;
+            final PackageManager pm = getPackageManager();
+            List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.MATCH_SYSTEM_ONLY);
+            String[][] dynamicPackages = new String [packages.size() - 1][2];
+
+            int count = 0;
+            for (int i = 0; i < packages.size(); i++) {
+                if (packages.get(i).packageName.equals("com.darryncampbell.disablesystemapps"))
+                    continue;   //  Would be a bad idea to disable ourselves
+                String packageDescription = packages.get(i).sourceDir;
+                if (packageDescription.endsWith("/"))
+                    packageDescription = packageDescription.substring(0, packageDescription.length() - 1);
+                if (packageDescription.contains("/"))
+                    packageDescription = packageDescription.substring(packageDescription.lastIndexOf('/') + 1, packageDescription.length());
+                dynamicPackages[count][0] = packageDescription;
+                dynamicPackages[count][1] = packages.get(i).packageName;
+                count++;
+            }
+            selectedPackages = dynamicPackages;
+            operating_system = "N/A - Applications are dynamically determined";
         }
-        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
-                Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP_MR1)
-        {
-            operating_system = "Lollipop 5.0";
-            selectedPackages = PackagesList.lollipop_50_packages;
-        }
-        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1 &&
-                Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-        {
-            operating_system = "Lollipop 5.1";
-            selectedPackages = PackagesList.lollipop_51_packages;
-        }
-        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
-        {
-            operating_system = "Marshmallow";
-            selectedPackages = PackagesList.marshmallow_packages;
-        }
-        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
-                Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1)
-        {
-            operating_system = "Nougat 7.0";
-            selectedPackages = PackagesList.nougat_70_packages;
-        }
-        else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1)
-        {
-            operating_system = "Nougat 7.1";
-            selectedPackages = PackagesList.nougat_71_packages;
-        }
-        else
-        {
-            operating_system = "UNRECOGNIZED";
+        else {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT &&
+                    Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                operating_system = "KitKat";
+                selectedPackages = PackagesList.kitkat_packages;
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
+                    Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP_MR1) {
+                operating_system = "Lollipop 5.0";
+                selectedPackages = PackagesList.lollipop_50_packages;
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1 &&
+                    Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                operating_system = "Lollipop 5.1";
+                selectedPackages = PackagesList.lollipop_51_packages;
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                    Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                operating_system = "Marshmallow";
+                selectedPackages = PackagesList.marshmallow_packages;
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
+                    Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
+                operating_system = "Nougat 7.0";
+                selectedPackages = PackagesList.nougat_70_packages;
+            } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
+                operating_system = "Nougat 7.1";
+                selectedPackages = PackagesList.nougat_71_packages;
+            } else {
+                operating_system = "UNRECOGNIZED";
+            }
         }
     }
 
